1b309a9eb7c0a4a11faf9a0c903b3a02294dcd2d
pabloandrei <pablo.andrei@gmail.com>
2020-02-06 15:08:29 -0300

    Fisrt code

---
+++ Logging/Program.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using System.IO;
+using Serilog.Events;
+using Serilog.Sinks.Elasticsearch;
+using Serilog.Settings.Configuration;
+using Serilog.Sinks.File;
+
+namespace Logging
+{
+    public class Program
+    {
+        public static readonly string AppName = typeof(Program).Namespace;
+
+        public static int Main(string[] args)
+        {
+            var configuration = GetConfiguration();
+
+            Log.Logger = CreateSerilogLogger(configuration);
+
+            try
+            {
+                Log.Information("Starting ({ApplicationContext})...", AppName);
+                System.Threading.Thread.Sleep(1000);
+                Log.Information("Stoping ({ApplicationContext})...", AppName);
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Program terminated unexpectedly ({ApplicationContext})!", AppName);
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        private static Serilog.ILogger CreateSerilogLogger(IConfiguration configuration)
+        {
+
+            var logstashUrl = configuration["Serilog:LogstashgUrl"];
+            return new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .Enrich.WithProperty("ApplicationContext", AppName)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(logstashUrl))
+                 {
+                     AutoRegisterTemplate = true,
+                     FailureCallback = e => Elastic_FailureCallback(e),
+                     EmitEventFailure = EmitEventFailureHandling.WriteToSelfLog |
+                                        EmitEventFailureHandling.WriteToFailureSink |
+                                        EmitEventFailureHandling.RaiseCallback
+                 })
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+        }
+
+        private static void Elastic_FailureCallback(object e)
+        {
+            Console.WriteLine("Unable to submit event "+e);
+        }
+ 
+        private static IConfiguration GetConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            var config = builder.Build();
+
+            return builder.Build();
+        }
+
+    }
+}
